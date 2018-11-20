e532de6e0d978a3efbb0fc97849e0d11c86c4288
Jonathan Reyes <holostriker@gmail.com>
2018-11-19 22:02:53 -0500

    Removing build/ from gitignore

---
+++ build/precache-manifest.2b49ee36c1184dd971b6c9b3d215a427.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "234869f398f4b0b5355a",
+    "url": "/ticker-search/static/js/runtime~main.234869f3.js"
+  },
+  {
+    "revision": "30d6489c6bb3b4a32739",
+    "url": "/ticker-search/static/js/main.30d6489c.chunk.js"
+  },
+  {
+    "revision": "9b32ee7a12f986932bf6",
+    "url": "/ticker-search/static/js/1.9b32ee7a.chunk.js"
+  },
+  {
+    "revision": "30d6489c6bb3b4a32739",
+    "url": "/ticker-search/static/css/main.57d77d1d.chunk.css"
+  },
+  {
+    "revision": "50ec3505fe1a03f47cafe6787bcb585a",
+    "url": "/ticker-search/index.html"
+  }
+];
