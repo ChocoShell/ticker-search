f90a2472aa871cd6bd276142779de7dd8ae5b0b2
Jonathan Reyes <holostriker@gmail.com>
2018-11-22 19:01:13 -0500

    adding build

---
+++ precache-manifest.a02925fb5038c12a9f25c7af64fa277f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "234869f398f4b0b5355a",
+    "url": "/ticker-search/static/js/runtime~main.234869f3.js"
+  },
+  {
+    "revision": "ee6b15bdaa301e3f7848",
+    "url": "/ticker-search/static/js/main.ee6b15bd.chunk.js"
+  },
+  {
+    "revision": "e0e43ead17ccd1889ecf",
+    "url": "/ticker-search/static/js/1.e0e43ead.chunk.js"
+  },
+  {
+    "revision": "ee6b15bdaa301e3f7848",
+    "url": "/ticker-search/static/css/main.bf1804be.chunk.css"
+  },
+  {
+    "revision": "9ef51e11e38b1de972fe572246bc4541",
+    "url": "/ticker-search/index.html"
+  }
+];
